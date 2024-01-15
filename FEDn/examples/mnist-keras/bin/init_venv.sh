096ba2e658f1c44e8c2c6480df4d7304395a8d24
Gandab <qendab@frontiers.io>
2024-01-15 04:45:42 +0200

    data created and splitted for mnist keras

--- FEDn/examples/mnist-keras/bin/init_venv.sh
+++ FEDn/examples/mnist-keras/bin/init_venv.sh
@@ -6,5 +6,5 @@ python3 -m venv .mnist-keras
 
 # Pip deps
 .mnist-keras/bin/pip install --upgrade pip
-.mnist-keras/bin/pip install -e ../../fedn
+# .mnist-keras/bin/pip install -e ../../fedn
 .mnist-keras/bin/pip install -r requirements.txt
