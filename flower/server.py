3e80123e3a34a9cd03613bb4ae9013ca506efc0e
Gandab Hasanova <qendabhesenova@gmail.com>
2024-07-26 14:04:54 +0300

    Update server.py

--- flower/server.py
+++ flower/server.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     # Start the Flower server
     fl.server.start_server(
         server_address="0.0.0.0:8080",
-        config=fl.server.ServerConfig(num_rounds=3),
+        config=fl.server.ServerConfig(num_rounds=10),
     )
